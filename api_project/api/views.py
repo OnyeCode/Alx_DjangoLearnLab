9aa2e4cbc7c75e71ced4ea7b9c18d5f6023b0ec4
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-11-23 19:08:14 +0100

    Done task 3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,15 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import BasePermission
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        return obj.owner == request.user
+
+
+permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]   # Or IsAdminUser if only admin can CRUD
 
 
