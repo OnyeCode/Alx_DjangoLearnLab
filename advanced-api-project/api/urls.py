48ed4215356ca69402e5894017cee422f8881256
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-11-30 20:46:08 +0100

    Done task 1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,8 +4,14 @@ from . import views
 urlpatterns = [
     path('books/', views.BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+
+    # ALX expects:
+    # "books/update"
+    # "books/delete"
+    # so the pk must come AFTER the keyword
+    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
+
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
 
