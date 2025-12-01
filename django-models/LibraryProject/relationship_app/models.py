05b03e0849cc326e10175b691bf1ea853eae5497
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-12-01 13:58:00 +0100

    Done task 0

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,14 +5,26 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return f"{self.name}."
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"{self.title} by {self.author}."
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return f"{self.name} library."
+
 class Librarian(models.Model):
     name = models.CharField(max_length=150)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} is the librarian of {self.library} library."
