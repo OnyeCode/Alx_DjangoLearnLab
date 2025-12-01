3cdc38765892af64bde81979462c81d2ce45c755
Chinemerem Okonkwo <gentycool@yahoo.com>
2025-12-01 19:49:13 +0100

    Done task 1

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('list_books/', views.list_books, name='list_books')
+	path('library_detail/', views.library_detail.as_view(), name='library_detail')
