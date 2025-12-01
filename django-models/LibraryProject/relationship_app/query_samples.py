26f0f0d89c3fccd73f8bbfd647bd0e621d6fd1a6
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-12-01 14:08:25 +0100

    Done task 0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,9 @@
 #Query all books by a specific author.
-Book.objects.filter(author=Author_name)
+Book.objects.filter(author=author_name)
 
 
 #List all books in a library.
-Library.objects.get(name=Library_name).books.all()
+Library.objects.get(name=library_name).books.all()
 
 
 #Retrieve the librarian for a library.
