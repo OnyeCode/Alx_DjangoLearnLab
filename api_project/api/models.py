891c44c85f50576fe80b4d487faa00b08c0cff59
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-12-04 23:17:59 +0100

    Done task 0

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.title
-
+class Book (models.Model):
+    title = models.CharField(max_length = 150)
+    author = models.CharField(max_length = 150)
