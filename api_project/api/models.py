e53a2d8c8ea8eb8993d7369a679888500165df24
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-12-05 00:44:13 +0100

    Done task 2

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Book (models.Model):
+class Book(models.Model):
     title = models.CharField(max_length = 150)
     author = models.CharField(max_length = 150)
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,7 +5,12 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
