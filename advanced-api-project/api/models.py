f5a23779f9c8abd836a9026bbc108c705d6d1b01
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-12-14 17:30:37 +0100

    Done task 0

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -7,7 +7,7 @@ class Author(models.Model):
 
 
 #The Book model with title field: a string field for the book’s title; publication_year field: an integer field for the year the book was published; author field: a foreign key linking to the Author model, establishing a one-to-many relationship from Author to Books
-class Book(models. Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
