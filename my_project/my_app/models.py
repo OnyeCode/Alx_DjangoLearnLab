9718a4b9557d0d459843ab42fa703d38c2a87756
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-12-29 02:07:50 +0100

    Added StudentProfile model

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -5,6 +5,17 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
+#StudentProfile
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=10)
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=20)
+    country = models.CharField(max_length=50)
+    education_level = models.CharField(max_length=50)
+
+
+
 """
 # Create your models here.
 
