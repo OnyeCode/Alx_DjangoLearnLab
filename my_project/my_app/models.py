88cf3e8b36aa5ff74fc0617a036b9456aee0fff5
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-12-29 02:05:08 +0100

    Added Django default User model

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+#User (Django’s default)
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+"""
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+
+'''
