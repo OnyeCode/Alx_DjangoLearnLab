b89cc4767c6caef0788d9379e00a55eb3fdbb131
Chinemerem Okonkwo <Engrchiok@gmail.com>
2026-01-01 16:30:33 +0100

    Updated User serializer

--- my_project/my_app/serializers.py
+++ my_project/my_app/serializers.py
@@ -12,10 +12,17 @@ from .models import (
 )
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        StudentProfile.objects.create(user=user)
+        return user
 
 class StudentProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
